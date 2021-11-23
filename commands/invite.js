cff5373073c110542719a9f0d81e3c21a0dda7d4
stickman009 <85288897+thesticky009@users.noreply.github.com>
2021-11-23 11:14:14 +0530

    Create invite.js

---
+++ commands/invite.js
@@ -0,0 +1,9 @@
+module.exports = {
+      name: "invite",
+      aliases: ['botinvite','getbot','botinv','inv','addbot','add'],
+      code: `$title[1;Invite Me;$getBotInvite]
+      $description[1;[Click Here to add me!]($getBotInvite)]
+      $color[1;RANDOM]`
+}
+
+//Returns your bot's invite link.
